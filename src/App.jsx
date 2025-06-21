0574218d5e88493c704c31011b63ff9d7fe01047
Rohit Kumar <rsbamil66@gamil.com>
2025-06-21 20:44:54 +0530

    upto hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
     <main>
       <Navbar />
+      <HeroSection />
+      <div className="h-dvh bg-black"></div>
     </main>
   );
 }

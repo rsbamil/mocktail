50dc60dac055ccd1e56b82137d0772d4d51a72d5
Rohit Kumar <rsbamil66@gamil.com>
2025-06-21 12:14:59 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-2xl text-indigo-300">Hello</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
 

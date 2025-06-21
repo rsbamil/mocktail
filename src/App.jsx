7a0ef0fa6e1b1def14e741c2b3d6829751b54c28
Rohit Kumar <rsbamil66@gamil.com>
2025-06-21 21:31:17 +0530

    complete hero

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <main>
       <Navbar />
       <HeroSection />
+      <Cocktails />
     </main>
   );
 }
